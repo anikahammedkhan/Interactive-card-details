2b821c8ab7c9ae6d602a1bcdd1d257b120340e9f
anikahammedkhan <anikkhan1105@gmail.com>
2022-09-20 01:45:56 +0600

    functionality added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-const nameField = document.getElementById('nameField');
-const nameValue = nameField.innerText;
-console.log(nameValue)
-const numField = document.getElementById('card-num');
-const numValue = numField.value;
-const months = document.getElementById('ccmonth');
-const monthsValue = months.value;
-const year = document.getElementById('ccyear');
-const yearValue = year.value;
-const cvv = document.getElementById('cvv');
-const cvvValue = cvv.value;
 document.getElementById('btn-confirm').addEventListener('click', function () {
-    console.log(nameValue, numValue, monthsValue, yearValue, cvvValue);
+    const name = document.getElementById('nameField').value;
+    const numValue = document.getElementById('card-num-input').value;
+    const months = document.getElementById('ccmonth').value;
+    const year = document.getElementById('ccyear').value;
+    const cvv = document.getElementById('cvv').value;
+    console.log(months, year, cvv)
+    document.getElementById('card-num').innerText = numValue;
+    document.getElementById('card-name').innerText = name;
+    document.getElementById('card-months').innerText = months;
+    document.getElementById('card-year').innerText = year;
+    document.getElementById('card-cvv').innerText = cvv;
 })
